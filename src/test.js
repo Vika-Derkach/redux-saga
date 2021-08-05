9956df54f4e11c476b7249a1a59f229d34f2b822
Vika-Derkach <queenviktoriyphp@gmail.com>
2021-08-05 17:00:04 +0300

    array

--- src/test.js
+++ src/test.js
@@ -1,10 +1,13 @@
 function* generatorFunction() {
-    for (let i = 0; i < 5; i++) {
-        yield i
-    }
+  for (let i = 0; i < 5; i++) {
+    yield i;
+  }
 }
 
-const iter = generatorFunction()
+const iter = generatorFunction();
+
+const num = [8, 7, 5, 0, 4];
+console.log(num);
 
 console.log(iter.next());
 console.log(iter.next());
